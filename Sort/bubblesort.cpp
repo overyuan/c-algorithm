35bb190a6bafb1b61df2de6245d5ae77a9e302cd
yuan <124112806+overyuan@users.noreply.github.com>
2024-11-11 19:47:08 +0800

    Create bubblesort.cpp

---
+++ Sort/bubblesort.cpp
@@ -0,0 +1,13 @@
+/*Bubble Sort*/
+//Bubble sort achieves sorting by successively comparing and exchanging neighbouring elements.
+void BubbleSort(vector<int> &nums){
+  int n = num.size();
+  for(int i=n-1;i>0;i--){
+    for(int j=0;j<i;j++){
+      if(nums[j]>nums[j+1]){
+        swap(nums[j],nums[j+1]);
+      }
+    }
+  }
+}
+//
