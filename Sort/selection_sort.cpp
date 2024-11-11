ce396b6af057a25c39a1bde0ff4569fb51116d43
yuan <124112806+overyuan@users.noreply.github.com>
2024-11-11 19:14:54 +0800

    Update selection_sort.cpp

--- Sort/selection_sort.cpp
+++ Sort/selection_sort.cpp
@@ -3,4 +3,13 @@
 vector<int> nums;
 void SeletionSort(vector<int> &nums){
   int n = nums.size();
-  
+  for(int i=0;i<=n-1;i++){
+    //找到最小元素
+    int k=i;
+    for(int j=i+1;j<=n-1;j++){
+      if(nums[j]<nums[i]) k=j;
+    }
+    swap(nums[k],nums[i]);//交换
+  }
+}
+//Time Complexity:O(n^2);Space Complexity:O(1);unstable.
