be53d752a1c898496a26045e2c2096c0a1df11b0
yuan <124112806+overyuan@users.noreply.github.com>
2024-11-11 11:42:28 +0800

    Create selection_sort.cpp

---
+++ Sort/selection_sort.cpp
@@ -0,0 +1,6 @@
+/*selection_sort*/
+//each round the smallest element is selected from the unsorted interval and placed at the end of the sorted interval.
+vector<int> nums;
+void SeletionSort(vector<int> &nums){
+  int n = nums.size();
+  
